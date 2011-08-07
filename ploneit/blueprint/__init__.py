4907f8128b84b379ca4c6d1b19c2a6faa3393d0a
Giorgio Borelli <casaborelli@gmail.com>
2011-08-07 15:33:52 +0200

    fixed get_file

--- ploneit/blueprint/__init__.py
+++ ploneit/blueprint/__init__.py
@@ -1,29 +1,27 @@
 import urllib
-# import cStringIO
-# from OFS.Image import File
 from xmlrpclib import Binary
 
 
-def get_file(item, base_field_name, url):
-    split_url = url.split('/')
-    split_url[2] = 'giorgio:giorgio@' + split_url[2]
-    url = '/'.join(split_url)
+def get_file(item, base_field_name, url, auth=None):
+    if auth:
+        split_url = url.split('/')
+        split_url[2] = '%s:%s@%s' % (auth[0], auth[1], split_url[2])
+        url = '/'.join(split_url)
     response = urllib.urlopen(url)
 
     # 'file': 'image content',
-    #  'file.filename': 'image.jpg',
-    #  'file.mimetype': 'image/jpeg',},
+    # 'file.filename': 'image.jpg',
+    # 'file.mimetype': 'image/jpeg',},
+    info = response.info()
+    file_name = ''
+    try:
+        file_name = info.dict['content-disposition'].split('=')[1].replace('"','')
+    except:
+        file_name = url.split('/')[-1]
 
-    # file_name = 'immagine.jpg'
-    # if url.endswith('leadImage'):
-    #     file_name = url.split('/')[-2] + '.' + response.info().type.split('/')[-1]
-    # else:
+    mimetype = info.type
 
-    file_name = url.split('/')[-1]
     item[base_field_name + '.filename'] = file_name
-    item[base_field_name + '.mimetype'] = 'image/jpeg'
+    item[base_field_name + '.mimetype'] = mimetype
     return Binary(response.read())
-
-    # return 
-    # return Binary(File(file_name, file_name, cStringIO.StringIO(response.read()).getvalue(), response.info().type))
 
