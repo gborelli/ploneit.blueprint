0a41eb6a25a415210c20c182f48660b840fe6c0b
Giorgio Borelli <casaborelli@gmail.com>
2011-08-07 13:49:31 +0200

    removed useless comments

--- ploneit/blueprint/blueprint.py
+++ ploneit/blueprint/blueprint.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 import simplejson
-# from datetime import datetime
-
 from zope.interface import classProvides, implements
 
 from collective.transmogrifier.interfaces import ISectionBlueprint, ISection
-# from collective.transmogrifier.utils import Expression
 
 
 class JsonImport(object):
@@ -14,15 +11,6 @@ class JsonImport(object):
 
     def __init__(self, transmogrifier, name, options, previous):
         self.previous = previous
-
-        # if options.get('keys_to_date'):
-        #     self.keys_to_date = Expression(options['keys_to_date'],
-        #                                    transmogrifier,
-        #                                    name,
-        #                                    options)(self) #bah
-        # else:
-        #     self.keys_to_date = ()
-
         file_ = open(options['filename'], 'r')
         self.reader = file_.read()
 
@@ -32,8 +20,4 @@ class JsonImport(object):
 
         data = simplejson.loads(self.reader, encoding='utf-8')
         for json_data in data:
-            # convertire in data ciò che mi serve
-            # for k, v in json_data.iteritems():
-            #     if v and k in self.keys_to_date:
-            #         json_data[k] = datetime.strptime(v, "%Y/%m/%d %H:%M:%S").isoformat()
             yield json_data
