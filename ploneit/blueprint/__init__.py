a830f8e9c02a095bff77a273862061baee81fee6
Giorgio Borelli <casaborelli@gmail.com>
2011-08-07 17:30:38 +0200

    changed get_file

--- ploneit/blueprint/__init__.py
+++ ploneit/blueprint/__init__.py
@@ -2,26 +2,7 @@ import urllib
 from xmlrpclib import Binary
 
 
-def get_file(item, base_field_name, url, auth=None):
-    if auth:
-        split_url = url.split('/')
-        split_url[2] = '%s:%s@%s' % (auth[0], auth[1], split_url[2])
-        url = '/'.join(split_url)
+def get_file(base_url, path):
+    url = '/'.join((base_url, path))
     response = urllib.urlopen(url)
-
-    # 'file': 'image content',
-    # 'file.filename': 'image.jpg',
-    # 'file.mimetype': 'image/jpeg',},
-    info = response.info()
-    file_name = ''
-    try:
-        file_name = info.dict['content-disposition'].split('=')[1].replace('"','')
-    except:
-        file_name = url.split('/')[-1]
-
-    mimetype = info.type
-
-    item[base_field_name + '.filename'] = file_name
-    item[base_field_name + '.mimetype'] = mimetype
     return Binary(response.read())
-
